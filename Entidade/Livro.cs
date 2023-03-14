a4539c9b3e8b713343e4af5cc6754987f1f4be5d
Andréa <andreamaragao@gmail.com>
2023-03-13 22:33:15 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Livro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto03.Entidade
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Projeto03.Entidade;
+using System;
+using System.Reflection.PortableExecutable;
+
+namespace Projeto03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro1;
+            livro1 = new Livro();
+            livro1.Autor = "Deus";
+            livro1.Titulo = "Biblia";
+
+            Livro livro2;
+            livro2 = new Livro();
+            livro2.Autor = "Joice";
+            livro2.Titulo = "Mulheres";
+
+
+            Livro livro3;
+            livro3 = new Livro();
+            livro3.Autor = "Lucinho";
+            livro3.Titulo = "Jovens";
+
+
+            Livro livro4 = new Livro();
+            livro4.Autor = "Paty";
+            livro4.Titulo = "Casamento";
+
+
+
+
+        }
+    }
+}
